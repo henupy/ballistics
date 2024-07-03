51aa20b9eb6fa575d1dacafcca4a70a4b3277c15
hhenu <henri.rapeli@gmail.com>
2024-07-03 23:03:50 +0300

    Added a separate script for trying to model rockets (vehicles with thrust)

---
+++ rocketsim.py
@@ -0,0 +1,15 @@
+"""
+Ballistic simulation for some kind of a rocket or missile or some shit. Takes into
+account the changing atmospheric properties as a function of altitude, and the Earth's
+curvature is also taken into account (or will be). The projectile can have some thrust,
+which can also be time-dependent. Plan is to take into account also the changing mass
+of the projectile (assuming that it loses mass while producing thrust).
+"""
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
